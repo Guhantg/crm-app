4baa4fcfe09804c94bbabe5c2931ef0326a1e14e
Guhan <guhantg.vkl@gmail.com>
2023-03-19 17:20:42 +0530

    Lead feature
    * Full lead functionality done and tested.

--- model/lead.py
+++ model/lead.py
@@ -1,1 +1,18 @@
-lead.py
+#lead model implementation
+
+
+#Insert lead query.
+def insert_lead_query();
+print 'Lead inserted into database'
+
+# Update lead query
+def update_lead_query();
+perint 'Lead updated into database'
+
+#get lead query
+def select_lead_query();
+print 'Lead select from database'
+
+#Delete Lead Query
+def delete_lead_query();
+ print 'Lead deleted from database.'
