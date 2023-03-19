048c9159d3b54323441cebdff719d489292cb28c
Guhan <35652560+Guhantg@users.noreply.github.com>
2023-03-19 17:32:12 +0530

    Lead feature (#1)
    * Full lead functionality done and tested.

--- controller/lead.py
+++ controller/lead.py
@@ -1,1 +1,19 @@
-lead.py
+# lead controller implementation
+
+
+# create lead
+def create_lead();
+ print 'Lead creation done.'
+
+
+#Update lead
+def update_lead();
+ print 'Lead update done.'
+
+ #get lead
+ def get_load();
+ print 'Get lead done.'
+
+ # Delete Lead
+ def delete_lead();
+ print 'Delete Leaed done.'
