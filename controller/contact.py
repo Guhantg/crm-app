6cd4c8ca052bf0b7fb11279110a4f84898fb361d
Guhan <guhantg.vkl@gmail.com>
2023-03-19 18:42:46 +0530

    Contact feature initialize
    * Functions for ontact feature create and Update

--- controller/contact.py
+++ controller/contact.py
@@ -1,1 +1,11 @@
-contact.py
+# Contact controller implementation
+
+
+# create contact
+def create_contact();
+ print 'contact creation done.'
+
+
+#Update contact
+def update_contact();
+ print 'contact update done.'
